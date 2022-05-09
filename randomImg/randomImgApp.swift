095dbf15ce93a82d9f819d4232c4de8ec7b214dc
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-09 11:25:17 +0200

    Initial Commit

---
+++ randomImg/randomImgApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomImgApp.swift
+//  randomImg
+//
+//  Created by Cloud Secrets on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct randomImgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
