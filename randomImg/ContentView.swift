d0f2050ed471ade001c8f9fd88e4515e67c34846
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-09 13:15:54 +0200

    first commit

--- randomImg/ContentView.swift
+++ randomImg/ContentView.swift
@@ -7,10 +7,60 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    @Published var image: Image?;
+    func fetchNewImage(){
+        guard let url = URL(string: "https://random.imagecdn.app/500/500")else{
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) {
+            data,_,_ in
+            guard let data = data else {return}
+            DispatchQueue.main.async {
+                guard let uiImage =  UIImage(data: data)else{return}
+                self.image = Image(uiImage: uiImage);
+            }
+        }
+        task.resume();
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel();
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                Spacer()
+                if let image = viewModel.image{
+                    image
+                        .resizable()
+                        .foregroundColor(Color.green)
+                        .frame(width: 250, height: 250)
+                        .padding()
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .foregroundColor(Color.green)
+                        .frame(width: 250, height: 250)
+                        .padding()
+                }
+                Spacer()
+                Button(action: {
+                    viewModel.fetchNewImage();
+                },
+                       label: {
+                    Text("Get New Image")
+                        .bold()
+                        .frame(width: 250, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(8)
+                        .padding()
+                    
+                })
+            }
+            .navigationTitle("Random Image")
+        }
     }
 }
 
